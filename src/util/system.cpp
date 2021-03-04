f9074ad2ee86ddb1cb515b22c8c0acd6cbdcc914
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2021-03-04 17:43:43 +0800

    update params and hash algo
    synced blockchain.

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -73,7 +73,7 @@
 // Application startup time (used for uptime calculation)
 const int64_t nStartupTime = GetTime();
 
-const char * const BITCOIN_CONF_FILENAME = "bitcoin.conf";
+const char * const BITCOIN_CONF_FILENAME = "zurcoin.conf";
 
 ArgsManager gArgs;
 
@@ -661,7 +661,7 @@ static std::string FormatException(const std::exception* pex, const char* pszThr
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(nullptr, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "bitcoin";
+    const char* pszModule = "zurcoin";
 #endif
     if (pex)
         return strprintf(
@@ -681,12 +681,12 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 fs::path GetDefaultDataDir()
 {
     // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Zurcoin
+    // Mac: ~/Library/Application Support/Zurcoin
+    // Unix: ~/.zurcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Zurcoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -696,10 +696,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Zurcoin";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".zurcoin";
 #endif
 #endif
 }
